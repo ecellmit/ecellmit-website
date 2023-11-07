d101d6aa8cd436be0cf7f2c4bfea02dd8643c7f6
Sagnik <santrasagnik10@gmail.com>
2023-11-08 04:25:01 +0530

    changed teams page

--- client/src/pages/teamPage.js
+++ client/src/pages/teamPage.js
@@ -1,50 +1,87 @@
-import React, { useEffect, useState } from "react"
-import TeamCard from "../components/TeamCard"
-import Loader from "./../components/Loader/Loader"
-
-import { useSpring, animated } from "react-spring";
+import React from "react";
+import TeamCard from "../components/TeamCard";
+import Loader from "./../components/Loader/Loader";
 
 const TeamPage = () => {
+  // Hardcoded team member data for 12 members
+  const hardcodedTeamData = [
+    {
+      name: "John Doe 1",
+      position: "President",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+      social: {
+        instagram: "john_doe_instagram1",
+        linkedin: "john_doe_linkedin1",
+        email: "john.doe1@example.com",
+      },
+    },
+    {
+        name: "John Doe 1",
+        position: "President",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        social: {
+          instagram: "john_doe_instagram1",
+          linkedin: "john_doe_linkedin1",
+          email: "john.doe1@example.com",
+        },
+      },
+      {
+        name: "John Doe 1",
+        position: "President",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        social: {
+          instagram: "john_doe_instagram1",
+          linkedin: "john_doe_linkedin1",
+          email: "john.doe1@example.com",
+        },
+      },
+      {
+        name: "John Doe 1",
+        position: "President",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+        social: {
+          instagram: "john_doe_instagram1",
+          linkedin: "john_doe_linkedin1",
+          email: "john.doe1@example.com",
+        },
+      },
+      {
+          name: "John Doe 1",
+          position: "President",
+          photo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+          social: {
+            instagram: "john_doe_instagram1",
+            linkedin: "john_doe_linkedin1",
+            email: "john.doe1@example.com",
+          },
+        },
+        {
+          name: "John Doe 1",
+          position: "President",
+          photo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+          social: {
+            instagram: "john_doe_instagram1",
+            linkedin: "john_doe_linkedin1",
+            email: "john.doe1@example.com",
+          },
+        },
+    // Add data for the other 11 members here...
+  ];
 
-    const [team, setTeam] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_SERVER_URL}/api/team/board`)
-            .then((res) => res.json())
-            .then((data) => {
-                setTeam(data.data)
-                setLoading(false)
-            })
-    }, [])
-
-    const slideSpring = useSpring({
-        transform: "translateY(0px)",
-        opacity: 1,
-        from: {
-            opacity: 0,
-            transform: "translateX(-100px)"
-        }   
-    })
-
-    return (
-        <div className="w-full min-vh-100 d-flex flex-column py-md-4 mx-auto align-items-center h-full mx-1 my-1">
-            <animated.h3 className="black-text text-white" style={{ ...slideSpring, fontSize: "2.7rem" }}>Meet the Team</animated.h3>
-            <div className="w-75 mt-5 d-flex flex-column mx-auto justify-content-center align-items-center h-full mx-1 my-1">
-            {
-                !loading ?
-                <div className="row gap-1 px-md-5">
-                    {
-                        team.map((member, index) => {
-                            return <TeamCard key={index} data={member}/>
-                        })
-                    }    
-                </div>
-                : <Loader />
-            }
-            </div>
+  return (
+    <div className="w-full min-vh-100 d-flex flex-column py-md-4 mx-auto align-items-center h-full mx-1 my-1">
+      <h3 className="black-text text-white" style={{ fontSize: "2.7rem" }}>
+        Meet the Team
+      </h3>
+      <div className="w-75 mt-5 d-flex flex-column mx-auto justify-content-center align-items-center h-full mx-1 my-1">
+        <div className="row gap-1 px-md-5">
+          {hardcodedTeamData.map((member, index) => (
+            <TeamCard key={index} data={member} />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TeamPage
+export default TeamPage;
